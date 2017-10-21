7783081144ed41963143a2f8d281394760c43640
triangleCZH <triangle@connect.hku.hk>
2017-10-21 23:15:38 +0800

    basic search tutor interface

--- MyTutor/Tutorial/views.py
+++ MyTutor/Tutorial/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+
 from django.views.generic import TemplateView
-from django.http import HttpResponse
-from .models import User
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from .models import Tutor, PrivateTutor
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -9,12 +11,16 @@ class HomePageView(TemplateView):
         return render(request, 'index.html', context=None)
 
 def index(request):
-	all_users = User.objects.all()
+	"""all_users = User.objects.all()
 	list = []
 	for user in all_users:
 		html = '<p>User {name} has username: {user_name} </b></p>'
 		list.append(html.format(name=user.name, user_name = user.user_name))
 	output = '<hr>'.join(list)
-	return HttpResponse(output)
+	return HttpResponse(output)"""
+	all_tutors = Tutor.objects.all()
+	private_tutors = PrivateTutor.objects.all()
+	params = {"latest_Tutor_list": all_tutors, "private_Tutor_list" : private_tutors}
+	return render(request, 'index.html', params)
 
 	
