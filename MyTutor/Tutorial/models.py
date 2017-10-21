60af3578a6b5c0169eee447014159988a4ff0df1
triangleCZH <triangle@connect.hku.hk>
2017-10-21 17:01:13 +0800

    model designed partly

--- MyTutor/Tutorial/models.py
+++ MyTutor/Tutorial/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+"""class User(models.Model):
+    user_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name"""
+
+class Student(models.Model):
+    user_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+class Tutor(models.Model):
+    user_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+class PrivateTutor
