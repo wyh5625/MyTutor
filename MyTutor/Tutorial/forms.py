3b2eac72d2c6902186b084b115bc74f6ff72a364
DaYujiaJulia <32391928+DaYujiaJulia@users.noreply.github.com>
2017-11-05 16:31:17 +0800

    :)

--- MyTutor/Tutorial/forms.py
+++ MyTutor/Tutorial/forms.py
@@ -33,6 +33,8 @@ class RegistrationForm(forms.Form):
             return password2
         raise forms.ValidationError('Passwords do not match.')
 
+class SearchForm(forms.Form):
+    keyWord = forms.CharField(label='Your name', max_length=100)
 
 
 
--- MyTutor/Tutorial/views.py
+++ MyTutor/Tutorial/views.py
@@ -13,6 +13,7 @@ import time
 from Tutorial.models import Tutor, PrivateTutor, ContractedTutor, MyUser, Notification, TutorialSession, Student, Tutor, Wallet
 from decimal import Decimal
 from django.template import RequestContext
+from .forms import SearchForm
 
 
 COMMISION = 1.05
@@ -273,3 +274,17 @@ def register_page(request):
         'registration/register.html',
         variables, RequestContext(request)
     )
+
+def search_filter(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SearchForm(request.POST)
+        # check whether it's valid:
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = SearchForm()
+
+    return render(request, 'index.html', {'form': form})
+
